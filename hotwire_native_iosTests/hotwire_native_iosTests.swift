197c238f0f8b80516a36e016e620b557ff50a37a
William Kennedy <williamkennedyjnr@gmail.com>
2024-10-11 14:12:11 +0100

    Initial Commit

---
+++ hotwire_native_iosTests/hotwire_native_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  hotwire_native_iosTests.swift
+//  hotwire_native_iosTests
+//
+//  Created by William Kennedy on 11/10/2024.
+//
+
+import Testing
+@testable import hotwire_native_ios
+
+struct hotwire_native_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
